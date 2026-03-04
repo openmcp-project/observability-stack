d943693c5f2c87729a758f5088c021eb3d9b5e45
René Schünemann <rene.schuenemann@sap.com>
2026-03-04 16:56:21 +0100

    add action

--- hack/push-component.sh
+++ hack/push-component.sh
@@ -12,12 +12,12 @@ detect_repo_root
 # Load component settings
 load_component_settings
 
-# Use REPOSITORY_CONTEXT from settings, or default
-if [ -z "$REPOSITORY_CONTEXT" ]; then
-    echo "Error: REPOSITORY_CONTEXT not set"
+# Use COMPONENTS_LOCATION from settings
+if [ -z "$COMPONENTS_LOCATION" ]; then
+    echo "Error: COMPONENTS_LOCATION not set"
     exit 1
 fi
 
 echo "Pushing component to $REPOSITORY_CONTEXT..."
-ocm transfer ctf --copy-resources --recursive --lookup ghcr.io/openmcp-project/components ./ctf "$REPOSITORY_CONTEXT"
+ocm transfer ctf --copy-resources --recursive ./ctf "$COMPONENTS_LOCATION"
 
