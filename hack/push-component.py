fa73e8114c8355dab06cbb44e064d1f231e2a026
René Schünemann <rene.schuenemann@sap.com>
2026-03-11 15:21:51 +0100

    try the no-update flag for pushing components

--- hack/push-component.py
+++ hack/push-component.py
@@ -43,6 +43,7 @@ def push_component(repo_root: Path, components_location: str) -> None:
     run_command([
         "ocm", "transfer", "ctf",
         "--copy-resources",
+        "--no-update",
         str(ctf_dir),
         components_location
     ])
